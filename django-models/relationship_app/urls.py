6d2fe942759332726ba2a2f77b18c25283fb8c0f
angelgracecy <angelgracecy@gmail.com>
2024-08-19 00:29:39 -0700

    django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -18,4 +18,15 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
+
 ]
+
+from django.urls import path
+from .views import add_book, edit_book, delete_book
+
+urlpatterns = [
+    path('add/', add_book, name='add_book'),
+    path('edit/<int:pk>/', edit_book, name='edit_book'),
+    path('delete/<int:pk>/', delete_book, name='delete_book'),
+]
+
