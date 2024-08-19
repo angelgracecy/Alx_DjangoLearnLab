3fcf3260d98e003f70a1de8fdbcb7f0f069ecba9
angelgracecy <angelgracecy@gmail.com>
2024-08-19 12:37:59 -0700

    django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -139,4 +139,37 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'member_view.html')
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+# views.py
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Welcome to the Admin view!")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return HttpResponse("Welcome to the Librarian view!")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("Welcome to the Member view!")
