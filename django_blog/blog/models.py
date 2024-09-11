397f3077490854248186ffc560ca363ab8385dfd
angelgracecy <angelgracecy@gmail.com>
2024-09-11 08:56:47 -0700

    myblog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -9,4 +9,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
 
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+def user_register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def user_profile(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated')
+    else:
+        form = CustomUserCreationForm(instance=request.user)
+    return render(request, 'profile.html', {'form': form})
+@login_required
+def user_profile(request):
+    if request.method == 'POST':
+        user_form = CustomUserCreationForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Profile updated')
+    else:
+        user_form = CustomUserCreationForm(instance=request.user)
+        profile_form = UserProfileForm(instance=request.user.userprofile)
+    return render(request, 'profile.html', {'user_form': user_form, 'profile_form': profile_form})
+
